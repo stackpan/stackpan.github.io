b40380ad57b6ab633aceac8b039d2ebf45ef0d48
Ivan Rizkyanto <ivanzkyanto@outlook.com>
2024-03-05 21:53:09 +0700

    Implement sroll to top button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './assets/styles.css';
+import { ChevronUpIcon } from '@heroicons/react/24/outline';
 import MenuItemList from './components/MenuItemList';
 import Header from './Header';
 import MainSection from './MainSection';
 import Footer from './Footer';
 
 function App() {
+  const [isBtnHidden, setIsBtnHidden] = useState(false);
+
+  useEffect(() => {
+    const homeElement = document.getElementById('home');
+
+    const handleScroll = () => {
+      setIsBtnHidden(window.scrollY <= homeElement.scrollHeight);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="drawer font-mono lg:text-lg selection:bg-secondary selection:text-secondary-content">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
@@ -16,6 +33,11 @@ function App() {
           <MainSection />
           <Footer />
         </div>
+        {(!isBtnHidden) && (
+          <a href="#home" className="btn btn-circle fixed bottom-4 right-4 btn-secondary">
+            <ChevronUpIcon className="w-6" />
+          </a>
+        )}
       </div>
       <div className="drawer-side z-20">
         <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay" />
